3872e9e554810745795b73c373ba69a2b9edf80c
Karoly Erdos <karoly@ebi.ac.uk>
2024-05-01 17:09:09 +0100

    Initial commit
    Source from https://github.com/spring-projects/spring-restdocs-samples/tree/main/restful-notes-spring-hateoas

---
+++ src/main/java/uk/ac/ebi/atlas/restfulnotesspringhateoas/RestfulNotesSpringHateoasApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.atlas.restfulnotesspringhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulNotesSpringHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulNotesSpringHateoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/ebi/atlas/restfulnotesspringhateoas/RestfulNotesSpringHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.atlas.restfulnotesspringhateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulNotesSpringHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
